bcd29a36e41f4028a43d10cafcfb521236c20d64
samiaul <samiaul.56@gmail.com>
2025-05-07 13:28:24 +0200

    Add method to IRootToken for retrieving parsing errors

--- src/main/java/com/vijie/core/interfaces/IRootToken.java
+++ src/main/java/com/vijie/core/interfaces/IRootToken.java
@@ -1,9 +1,14 @@
 package com.vijie.core.interfaces;
 
+import com.vijie.core.errors.GenericUnparsedError;
+
 /**
  * Represents the root token of the parser tree.
  *
  * @param <V> the type of the value held by this root token
  */
 public interface IRootToken<V> extends ICompositeToken<V> {
+
+    GenericUnparsedError[] getErrors();
+
 }
